b41e8a9a2a6d01400efdcb2ed713c9d5db386a97
jaloliddingulomov <jalolidding98@gmail.com>
2025-10-06 23:35:34 +0200

    initialize firebase

--- composeApp/src/androidMain/kotlin/project/play/ground/jg/AndroidApp.kt
+++ composeApp/src/androidMain/kotlin/project/play/ground/jg/AndroidApp.kt
@@ -1,16 +1,21 @@
 package project.play.ground.jg
 
 import android.app.Application
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.initialize
+import di.firebaseModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class AndroidApp: Application() {
+class AndroidApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        Firebase.initialize(this)
         startKoin {
             androidContext(this@AndroidApp)
             androidLogger()
+            modules(firebaseModule)
         }
     }
 }
