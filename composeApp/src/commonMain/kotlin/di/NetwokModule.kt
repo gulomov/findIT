19e2d14896c33b3ad95547c14f324ad443109d79
jaloliddingulomov <jalolidding98@gmail.com>
2025-10-08 00:44:11 +0200

    add provideHttpClientModule

---
+++ composeApp/src/commonMain/kotlin/di/NetwokModule.kt
@@ -0,0 +1,18 @@
+package di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.http.ContentType
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val provideHttpClientModule = module {
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json(json = Json { ignoreUnknownKeys = true }, contentType = ContentType.Any)
+            }
+        }
+    }
+}
