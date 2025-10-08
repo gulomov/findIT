595e85401e2473b1bd923c2ad3522ac8b82bb5e5
jaloliddingulomov <jalolidding98@gmail.com>
2025-10-09 00:23:34 +0200

    rename package name

--- composeApp/src/commonMain/kotlin/navigation/MainGraph.kt
+++ composeApp/src/commonMain/kotlin/navigation/MainGraph.kt
@@ -6,7 +6,7 @@ import androidx.compose.animation.core.tween
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import feature.home.HomeScreen
+import ui.home.HomeScreen
 
 fun NavGraphBuilder.mainGraph(navController: NavController) {
     composable<HomeScreen>(
