74e0f9423016d735f1d77a1cd1c902f984a4ff6e
jaloliddingulomov <jalolidding98@gmail.com>
2025-10-09 00:23:16 +0200

    use include instead of listOf

--- composeApp/src/commonMain/kotlin/data/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/data/di/DataModule.kt
@@ -4,7 +4,5 @@ import data.feature.recommendations.di.recommendationsModule
 import org.koin.dsl.module
 
 val dataModule = module {
-    listOf(
-        recommendationsModule
-    )
+    includes(recommendationsModule)
 }
--- composeApp/src/commonMain/kotlin/domain/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/domain/di/DomainModule.kt
@@ -4,5 +4,5 @@ import domain.feature.recommendations.di.recommendationsModule
 import org.koin.dsl.module
 
 val domainModule = module {
-    listOf(recommendationsModule)
+    includes(recommendationsModule)
 }
