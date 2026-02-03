272839948a8c58c0f3d12edec5b5149169004768
jaloliddingulomov <jalolidding98@gmail.com>
2026-02-03 23:01:51 +0100

    refactor imports to include feature.di.featureViewModel in AndroidApp.kt

---
+++ composeApp/src/commonMain/kotlin/feature/di/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package feature.di
+
+import feature.home.HomeViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModelOf(::HomeViewModel)
+}
